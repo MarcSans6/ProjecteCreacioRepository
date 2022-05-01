8aadcacc1412ad89a529f57ac304feeb5569f14d
AleixFerriz <aferriz@edu.tecnocampus.cat>
2022-05-01 17:40:17 +0200

    Animació moviment dreta i esquerra però no es deixa de moure

--- ProjecteCreacio/Assets/_Scripts/Movement.cs
+++ ProjecteCreacio/Assets/_Scripts/Movement.cs
@@ -6,12 +6,14 @@ public class Movement : MonoBehaviour
 {
     public float RunSpeed = 3f;
     public SpriteRenderer spriteRenderer;
-
+    public Animator playerAnimator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -20,19 +22,23 @@ public class Movement : MonoBehaviour
         {
             transform.Translate(new Vector2(RunSpeed * Time.deltaTime,0), Space.World);
             spriteRenderer.flipX = false;
+            playerAnimator.SetBool("Run", true);
         }
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
             transform.Translate(new Vector2(-RunSpeed * Time.deltaTime, 0), Space.World);
             spriteRenderer.flipX = true;
+            playerAnimator.SetBool("Run", true);
         }
         if (Input.GetKey("w") || Input.GetKey("up"))
         {
             transform.Translate(new Vector2(0, RunSpeed * Time.deltaTime), Space.World);
+            playerAnimator.SetBool("Run", true);
         }
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
             transform.Translate(new Vector2(0, -RunSpeed * Time.deltaTime), Space.World);
+            playerAnimator.SetBool("Run", true);
         }
 
     }
