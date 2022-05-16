fb4c85d4c98529a8d233e0a86a26a47fa4eb91d2
AleixFerriz <aferriz@edu.tecnocampus.cat>
2022-05-16 19:29:01 +0200

    Bala

---
+++ ProjecteCreacio/Assets/_Scripts/DisparoPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform controladorDisparo;
+    [SerializeField] private GameObject bala;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Disparar();
+        }
+    }
+
+    private void Disparar()
+    {
+        Instantiate(bala, controladorDisparo.position, controladorDisparo.rotation);
+    }
+}
