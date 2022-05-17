f0e4c10ce2913d7024619869cde0fa030412581b
AleixFerriz <aferriz@edu.tecnocampus.cat>
2022-05-17 19:38:18 +0200

    EnemyDamage

--- ProjecteCreacio/Assets/_Scripts/Enemies/EnemyDamage.cs
+++ ProjecteCreacio/Assets/_Scripts/Enemies/EnemyDamage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IShootable
+public class EnemyDamage : MonoBehaviour, IShootable
 {
     public void TakeDamage(float amount)
     {
