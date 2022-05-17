0dc6d0a2f39ec0a743490387ede813ddd546c40b
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2022-05-17 19:01:20 +0200

    yes

--- ProjecteCreacio/Assets/_Scripts/Enemies/State.cs
+++ ProjecteCreacio/Assets/_Scripts/Enemies/State.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemies : MonoBehaviour
+public class State
 {
-    enum state
-    {
-        Idle, Wander, Attack
-    }
+    public Action OnEnter;
+    public Action OnStay;
+    public Action OnExit;
     // Start is called before the first frame update
     void Start()
     {
