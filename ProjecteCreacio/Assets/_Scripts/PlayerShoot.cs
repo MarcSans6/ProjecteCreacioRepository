de101a9393befa996ba058b6ce0cfc9441f4d3de
AleixFerriz <aferriz@edu.tecnocampus.cat>
2022-05-30 19:32:10 +0200

    a

--- ProjecteCreacio/Assets/_Scripts/PlayerShoot.cs
+++ ProjecteCreacio/Assets/_Scripts/PlayerShoot.cs
@@ -5,33 +5,55 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public Transform shotControler;
     public GameObject bullet;
+    private Rigidbody2D bulletRigidbody;
+    private BulletController bulletController;
 
-    public bool isShooting;
-    public float chargeTime = 0.5f;
-    private float lastTimeFire;
 
-    
+    private bool m_IsShooting;
+    public float m_BulletChargeTime = 0.5f;
+    private float m_LastTimeFire;
+
+    public bool isShooting
+    {
+        get => m_IsShooting;
+        set => m_IsShooting = value;
+    }
+    public float BulletChargeTime
+    {
+        get => m_BulletChargeTime;
+        set => m_BulletChargeTime = value;
+    }
+    public float LastTimeFire
+    {
+        get => m_LastTimeFire;
+        set => m_LastTimeFire = value;
+    }
+
+    public void Start()
+    {
+        bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletController = bullet.GetComponent<BulletController>();
+    }
     public void Update()
     {
-        if (isShooting && lastTimeFire + chargeTime <= Time.time)
+        if (m_IsShooting && m_LastTimeFire + m_BulletChargeTime <= Time.time)
         {
-            Debug.Log("Shoot");
+            Debug.Log("Player is shooting");
             CreateBullet();
         }
     }
     public void OnShootStart()
     {
-        isShooting = true;
+        m_IsShooting = true;
     }
     public void OnShootEnd()
     {
-        isShooting = false;
+        m_IsShooting = false;
     }
     private void CreateBullet()
     {
-        Instantiate(bullet, shotControler.position, shotControler.rotation);
-        lastTimeFire = Time.time;
+        Instantiate(bullet, bulletController.position, Quaternion.AngleAxis(bulletController.rotation, Vector3.forward));
+        m_LastTimeFire = Time.time;
     }
 }
