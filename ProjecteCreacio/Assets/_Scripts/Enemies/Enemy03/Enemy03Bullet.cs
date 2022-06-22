e9a4d0431670f581c860db80f16ca05224c6d734
AleixFerriz <aferriz@edu.tecnocampus.cat>
2022-06-22 18:21:41 +0200

    Enemy03 com el 01, falta acabar

---
+++ ProjecteCreacio/Assets/_Scripts/Enemies/Enemy03/Enemy03Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy03Bullet : MonoBehaviour
+{
+    private Transform player;
+    private Transform enemy;
+    private Rigidbody2D _rigidbody;
+    public float Speed = 10;
+    Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy03").GetComponent<Transform>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        dir = player.position - enemy.position;
+        dir.Normalize();
+
+        SetVelocity(dir);
+    }
+
+    void SetVelocity(Vector3 dir)
+    {
+        _rigidbody.velocity = dir * Speed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ ProjecteCreacio/Assets/_Scripts/Enemies/Enemy03/Enemy03BulletShooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy03BulletShooting : MonoBehaviour
+{
+    public Transform EnemyShotController;
+    public GameObject EnemyBulletPrefab;
+
+    private InteligenceEnemy inteligenceEnemy;
+
+    public float chargeTime = 1.0f;
+    private float lastTimeFire;
+    private void Start()
+    {
+        inteligenceEnemy = GetComponent<InteligenceEnemy>();
+    }
+
+    void Update()
+    {
+        if (inteligenceEnemy.isInAttackRange && lastTimeFire + chargeTime <= Time.time)
+        {
+            CreateEnemyBullet();
+        }
+    }
+
+    private void CreateEnemyBullet()
+    {
+        Instantiate(EnemyBulletPrefab, EnemyShotController.transform.position, EnemyShotController.rotation);
+        lastTimeFire = Time.time;
+    }
+}
