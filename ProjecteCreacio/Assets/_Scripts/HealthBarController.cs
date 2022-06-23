27a74d380083fd26d0c5a12d1c28340f3ec50509
MarcSans6 <msansc@ecu.tecnocampus.cat>
2022-06-23 20:38:42 +0200

    commit final

--- ProjecteCreacio/Assets/_Scripts/HealthBarController.cs
+++ ProjecteCreacio/Assets/_Scripts/HealthBarController.cs
@@ -7,17 +7,18 @@ public class HealthBarController : MonoBehaviour
     private HealthSystem healthSystem;
     public SpriteRenderer childrenSpriteRenderer;
 
-    public Sprite h00, h01, h02, h03, h04, h05, h06, h07, h08, h09;
+    public Sprite h00, h01, h02, h03, h04, h05, h06, h07, h08, h09, h10;
     private Sprite[] healthBarArray;
     void Start()
     {
+        h10 = h09;
         healthSystem = GetComponentInParent<HealthSystem>();
-        healthBarArray = new Sprite[] { h00, h01, h02, h03, h04, h05, h06, h07, h08, h09};
+        healthBarArray = new Sprite[] { h00, h01, h02, h03, h04, h05, h06, h07, h08, h09, h10};
     }
 
     // Update is called once per frame
     void Update()
     {
-        childrenSpriteRenderer.sprite = healthBarArray[(int)healthSystem.CurrentHealth - 1];
+        childrenSpriteRenderer.sprite = healthBarArray[(int)healthSystem.CurrentHealth];
     }
 }
