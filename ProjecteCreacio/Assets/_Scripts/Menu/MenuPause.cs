994c4d4087fa68552f3c1c0141979ad8791f5e4b
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2022-06-23 20:44:14 +0200

    ss

--- ProjecteCreacio/Assets/_Scripts/Menu/MenuPause.cs
+++ ProjecteCreacio/Assets/_Scripts/Menu/MenuPause.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
     [SerializeField] private GameObject buttonPause;
     [SerializeField] private GameObject menuPause;
+    [SerializeField] private GameObject menuButton;
+    [SerializeField] private GameObject OptionPanel;
+    [SerializeField] private AudioMixer audioMixer;
 
+
+
+    private void Start()
+    {
+        OptionPanel.SetActive(false);
+    }
     public void Pause()
     {
         Time.timeScale = 0f;
@@ -28,6 +38,26 @@ public class MenuPause : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnToMenu(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Options ()
+    {
+        OptionPanel.SetActive(true);
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void ChangeQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
     }
 
     public void Quit()
