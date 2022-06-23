d3502eb04680edbc0711d111f2ce87263afb08e2
MarcSans6 <msansc@ecu.tecnocampus.cat>
2022-06-23 16:21:13 +0200

    Scene manager en construccion...

---
+++ ProjecteCreacio/Assets/_Scripts/ExitController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    private bool m_OnTrigger;
+
+    public bool OnTrigger
+    {
+        get => m_OnTrigger;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<PlayerMovement>();
+        if (player != null)
+        {
+            m_OnTrigger = true;
+        }
+    }
+}
